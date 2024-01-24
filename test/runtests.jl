c11ffe16cbaafa442d8bd5667461cee3b7d12f73
Pratyai Mazumder <pratyai.mazumder@gmail.com>
2024-01-24 15:19:29 +0100

    format

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,20 @@
-using Test; using Laplacians; using MAT; using LinearAlgebra; using BenchmarkTools
+using Test;
+using Laplacians;
+using MAT;
+using LinearAlgebra;
+using BenchmarkTools;
 include("../src/CombinatorialMultigrid.jl");
 
 using Main.CombinatorialMultigrid
 
 ## load example matrix
 
-file = matopen("../example/X.mat"); X = read(file, "X"); close(file)
+file = matopen("../example/X.mat");
+X = read(file, "X");
+close(file);
 LX = lap(X);
 b1 = rand(Float64, size(X, 1));
-b1 = b1 .- sum(b1)/length(b1);
+b1 = b1 .- sum(b1) / length(b1);
 
 @info "###  Running CMG ###"
 t = @elapsed (pfunc, h) = cmg_preconditioner_lap(LX);
@@ -18,13 +24,21 @@ t = @elapsed x = pfunc(b1);
 
 @info "###  Running PCG with CMG    ### "
 ## solve with pcg and cmg preconditioner
-f1 = pcgSolver(LX,pfunc);
-t = @elapsed x = f1(b1, maxits=40, tol=1e-6,verbose=true);
+f1 = pcgSolver(LX, pfunc);
+t = @elapsed x = f1(b1, maxits = 40, tol = 1e-6, verbose = true);
 @info "Time Required to solve system: $(t) seconds"
 
 ## solve with approxchol_lap from laplacians
 @info "###  Running approxchol_lap    ###"
-t = @elapsed solver = approxchol_lap(X; tol=1e-6, maxits=1000, maxtime=Inf, verbose=true, pcgIts=Int[], params=ApproxCholParams());
+t = @elapsed solver = approxchol_lap(
+  X;
+  tol = 1e-6,
+  maxits = 1000,
+  maxtime = Inf,
+  verbose = true,
+  pcgIts = Int[],
+  params = ApproxCholParams(),
+);
 @info "Time Required to build Lap Solver: $(t) seconds"
 t = @elapsed x = solver(b1);
 @info "Time Required to find x: $(t) seconds"
